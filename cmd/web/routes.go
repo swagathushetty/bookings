ff0b222d386d52ea4d4023bcb138cf88481e97a0
swagath shetty <swagath.shetty@gmail.com>
2021-08-15 12:46:13 +0530

    added static file support and images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,6 +21,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	//file server to fetch static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
 
